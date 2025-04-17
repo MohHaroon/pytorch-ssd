d39f0cf155e104e9b1c6a5e085ad11705dbe870a
Mohamed Haroon <116776845+MohHaroon@users.noreply.github.com>
2025-04-18 00:29:51 +0530

    Update voc_dataset.py

--- vision/datasets/voc_dataset.py
+++ vision/datasets/voc_dataset.py
@@ -51,7 +51,7 @@ class VOCDataset:
         self.class_dict = {class_name: i for i, class_name in enumerate(self.class_names)}
 
     def __getitem__(self, index):
-       image_id = self.ids[index]
+        image_id = self.ids[index]
         boxes, labels, is_difficult = self._get_annotation(image_id)
     
         if not self.keep_difficult:
